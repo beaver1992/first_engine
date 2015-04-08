9243308abf4736439b1b302c9773f2b786ce492f
Ivan <psyxoz228@gmail.com>
2015-04-08 15:56:15 +0300

    continuous learn engine(engine)

--- app/models/blorgh/post.rb
+++ app/models/blorgh/post.rb
@@ -1,5 +1,17 @@
 module Blorgh
   class Post < ActiveRecord::Base
-    has_many :comments
+    attr_accessor :author_name
+    belongs_to :author, class_name: Blorgh.author_class.to_s
+    before_save :set_author
+    has_many :comments, dependent: :destroy
+
+    def summary
+      "#{title}"
+    end
+
+    private
+      def set_author
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
+      end
   end
 end
